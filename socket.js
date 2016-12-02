792aa1c73305d262bef8ef5d300e90adecb420b0
Exponent500 <reuvenkishon@gmail.com>
2016-12-02 13:24:18 -0800

    refactored add user listener

--- socket.js
+++ socket.js
@@ -8,10 +8,9 @@ module.exports = function(app,io){
 
 	  socket.room = [];
 	  
-	  // show that a user connected when a connection event occurs
 	  console.log('a user connected', socket.id);
 
-	  // handler for a client socket disconnect
+	  // socket.io listener for when a client disconnects
 	  socket.on('disconnect', function(){
 	    console.log('user disconnected', socket.userid);
 	    // grab the index where the specific user's id is located within the global users array
@@ -26,21 +25,19 @@ module.exports = function(app,io){
 	    	socket.leave(socket.room[i]);	
 	    }
 	    
-	    // socket.disconnect();
 	    console.log(users);
 
 	  });
 
-	  // handler for when a chat message comes in from the app
+	  // socket.io listener for when a chat message comes in from a client
 	  socket.on('chat message', function(msg){
-	  	// send chat message to everyone except for who sent it
+	  	// send chat message to all clients except the client that sent the message
 	  	socket.broadcast.to(msg.chatid).emit('chat message', msg);
-		// send chat message to everyone, including who sent it
-	  	//io.sockets.in(msg.messagerecipient_id).emit('chat message', msg);
 	    console.log(msg.chatid);
 	  });
 
-	  // socket.io listener for client subscribe event, which is when a client asks to join a room
+	  // socket.io listener that adds client to local users list and all rooms the client is 
+	  // subscribed to. It also lets all other clients know that it has logged in.
 	  socket.on('add user', function(msg) {
 	
 		// assign the userid to the socket object
@@ -60,10 +57,9 @@ module.exports = function(app,io){
 	  	console.log(socket.userid);
 	  	console.log(socket.room);
 
-	  	// send all clients the notification that this client has logged in
-	  	io.sockets.emit('logged in', users);
-	  	// broadcast to all clients in this room that this specific user has joined
-	  	//socket.broadcast.to(msg.chatid).emit('logged in', {username: msg.username, chatgroupId: msg.chatid});
+	  	// send all clients the notification that this client has logged in and pass along the local
+	  	// list of users
+	  	io.sockets.emit('logged in', {users:users, userLoggedIn: msg.userid});
 
 	  });
 
